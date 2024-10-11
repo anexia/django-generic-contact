eb3a9cfd973fc191640de8e25e9d75a7d4991667
Bernhard Hagmann <mail@bernhard-hagmann.com>
2024-10-11 12:56:49 +0200

    SIANXKE-389: Update Python, Django, and package versions

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-generic-contact",
-    version=os.getenv("PACKAGE_VERSION", "1.0.0").replace("refs/tags/", ""),
+    version=os.getenv("PACKAGE_VERSION", "1.1.0").replace("refs/tags/", ""),
     packages=find_packages(),
     include_package_data=True,
     license="MIT License",
@@ -24,17 +24,18 @@ setup(
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Framework :: Django",
-        "Framework :: Django :: 3.2",
-        "Framework :: Django :: 4.1",
         "Framework :: Django :: 4.2",
+        "Framework :: Django :: 5.0",
+        "Framework :: Django :: 5.1",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
     ],
 )
